315835c75e77b84d0da44c6bd6f30a7283a4858f
ZALLE Youssouf <goulkamz@gmail.com>
2026-07-14 10:01:14 +0000

    dockerisation ...

--- src/main/java/com/immobilier/gestionImmobiliere/configurations/security/WebSecurityConfig.java
+++ src/main/java/com/immobilier/gestionImmobiliere/configurations/security/WebSecurityConfig.java
@@ -71,8 +71,8 @@ public class WebSecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(
                         auth ->
-                                auth.requestMatchers("/auth/**").permitAll()
-                                        .requestMatchers("/public/**").permitAll()
+                                auth.requestMatchers("/api/auth/**").permitAll()
+                                        .requestMatchers("/api/public/**").permitAll()
                                         .anyRequest().authenticated()
                 );
 
