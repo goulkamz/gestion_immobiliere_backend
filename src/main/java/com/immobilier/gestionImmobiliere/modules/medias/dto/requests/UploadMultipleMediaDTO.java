296bed1bd0540e2c52e195713903a7c0f3d665fa
ZALLE Youssouf <goulkamz@gmail.com>
2026-09-14 14:21:05 +0000

    upload +eurs medias ...

--- src/main/java/com/immobilier/gestionImmobiliere/modules/medias/dto/requests/UploadMultipleMediaDTO.java
+++ src/main/java/com/immobilier/gestionImmobiliere/modules/medias/dto/requests/UploadMultipleMediaDTO.java
@@ -3,6 +3,7 @@ package com.immobilier.gestionImmobiliere.modules.medias.dto.requests;
 import com.immobilier.gestionImmobiliere.donnees.medias.model.TypeEntiteMedia;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,6 +13,8 @@ import java.util.List;
 public class UploadMultipleMediaDTO {
     @NotNull private TypeEntiteMedia entiteType;
     @NotNull private Integer entiteId;
-    @NotEmpty private List<MultipartFile> fichiers;
+    @NotEmpty(message = "Au moins un fichier est requis")
+    @Size(max = 10, message = "Maximum 10 fichiers par envoi")
+    private List<MultipartFile> fichiers;
     private Boolean isPrincipal; // s'applique uniquement au premier fichier du lot, si demandé
 }
